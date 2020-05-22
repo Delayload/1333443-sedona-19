dc5f183d4a126184f14dbfb7fb12fb089a338e1a
delayload <alexjaworski88@gmail.com>
2020-05-22 03:56:50 +0200

    Завершаем вёрстку личного проекта

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -5,10 +5,12 @@ toggle.addEventListener("click", function(evt) {
   evt.preventDefault();
   if (toggle.classList.contains("main-nav__toggle--active")) {
     toggle.classList.remove("main-nav__toggle--active");
+    toggle.classList.add("main-nav__toggle--close");
     list.classList.remove("main-nav__list--active");
   }
   else {
     toggle.classList.add("main-nav__toggle--active");
+    toggle.classList.remove("main-nav__toggle--close");
     list.classList.add("main-nav__list--active");
   }
 });
